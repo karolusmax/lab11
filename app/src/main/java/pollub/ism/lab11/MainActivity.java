d1ad47cbb8d5013c242687090f255e574c17c007
karolusmax <kmaksymiuk5555@gmail.com>
2021-06-01 13:24:53 +0200

    main activity layout added

--- app/src/main/java/pollub/ism/lab11/MainActivity.java
+++ app/src/main/java/pollub/ism/lab11/MainActivity.java
@@ -1,8 +1,14 @@
 package pollub.ism.lab11;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemSettings: startActivity(new Intent(this,SettingsActivity.class)); return true;
+            case R.id.itemExit: finishAndRemoveTask(); return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+    public void clickStart(View view) {
+        Toast.makeText(this,"Start", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickStop(View view) {
+        Toast.makeText(this,"Stop",Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickRestart(View view) {
+        clickStop(view);
+        clickStart(view);
     }
 }
